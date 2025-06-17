f4b0fe7a111ca445fbc285fa1638059456aa299b
TheoW03 <21theowillis@gmail.com>
2025-06-17 12:17:44 -0400

    fixed mapper
    now its 16 kb

--- src/main.s
+++ src/main.s
@@ -10,7 +10,7 @@
   ; .byte "NES", $1A      ; iNES header identifier
   .byte $4E, $45, $53, $1A
   .byte 2               ; 2x 16KB PRG code
-  .byte 2               ; 1x  8KB CHR data
+  .byte 1               ; 1x  8KB CHR data
   .byte $01, $00        ; mapper 0, vertical mirroring
 .segment "VECTORS"
   ;; When an NMI happens (once per frame if enabled) the label nmi:
@@ -144,7 +144,7 @@ nmi:
                 jsr handle_paddle1_controls
                 jsr handle_paddle2_controls
         
-                ; update ball
+                ; update l
                 jsr check_collisons
                 jsr update_ball
                 jsr check_point
